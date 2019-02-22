fedffce3851a33d49edfb024bbbfa00004d2ac76
Gill Abada <gill@callifi.com>
2019-02-22 11:49:35 -0500

    add key to cards.

--- lambdatimes/src/components/Content/Cards.js
+++ lambdatimes/src/components/Content/Cards.js
@@ -6,8 +6,8 @@ const Cards = props => {
     <div className="cards-container">
       {/* Using the cards prop, map over the list creating a 
           new Card component for each passing the card as the only prop*/}
-          {props.cards.map((card) => (
-                <div>
+          {props.cards.map((card, index) => (
+                <div key={index}>
                     <Card card={card}/>
                 </div>
                 ))}
