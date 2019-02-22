d315b6cde2e3bf53d8a0fd76868b6f3ff2546033
Gill Abada <gill@callifi.com>
2019-02-22 12:39:19 -0500

    add prop types to cards, card, tab, and tabs.

--- lambdatimes/src/components/Content/Tabs.js
+++ lambdatimes/src/components/Content/Tabs.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import Tab from './Tab';
+import PropTypes from 'prop-types';
+
 const Tabs = props => {
   return (
     <div className="tabs">
@@ -18,4 +20,8 @@ give the tab component a `selectTabHandler`, the `selectedTab`, and the `tab` it
 };
 
 // Make sure to use PropTypes to validate your types!
+Tabs.propTypes = {
+  tabs: PropTypes.array
+};
+
 export default Tabs;
