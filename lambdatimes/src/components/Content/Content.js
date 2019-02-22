7fd605bed15692e3086886080ad764556c1e02df
Gill Abada <gill@callifi.com>
2019-02-22 12:24:57 -0500

    complete filterCards function.

--- lambdatimes/src/components/Content/Content.js
+++ lambdatimes/src/components/Content/Content.js
@@ -44,7 +44,12 @@ export default class Content extends Component {
           of the items from cardData. 
         - else, it should only return those cards whose 'tab' matched this.state.selected.
     */
-    return this.state.cards;
+    if (this.state.selected === 'all') {
+      return this.state.cards;
+    } else {
+      const cards = [...this.state.cards];
+      return cards.filter(card => card.tab === this.state.selected);
+    }
   };
 
   render() {
